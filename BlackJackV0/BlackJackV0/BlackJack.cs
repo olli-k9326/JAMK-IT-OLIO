093545b8d61b5b498142970bc34f95b2056151b7
K9326 <K9326@ICT-421-WS-17>
2017-01-11 14:41:58 +0200

    BlackJack work

--- BlackJackV0/BlackJackV0/BlackJack.cs
+++ BlackJackV0/BlackJackV0/BlackJack.cs
@@ -9,26 +9,56 @@ namespace JAMK.IT
     static void Main()
     {
       int myNumber = 17;
-      int theirNumber;
+      int theirNumber = 0;
+      string input;
       System.Console.WriteLine("*** BlackJack! ***");
-      System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-      //reading and converting 
-      theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-      //comparing that given umber is valid
-      if (theirNumber < 1 || theirNumber > 21)
+      while(true)
       {
-        Console.WriteLine("The given number is out of limits, try again.");
-      }
-      else {
-        //comparing
-        if (theirNumber >= myNumber && theirNumber <= 21)
-        {
-          System.Console.WriteLine("You win.");
+                System.Console.Write("\nCan you beat my number? Enter any number between 1-21: ");
+                //reading and converting 
+                while (true)
+                {
+                    input = System.Console.ReadLine();          // kysytään syöte
+                    if (Int32.TryParse(input, out theirNumber) == false)     // onko kokonaisluku, jos ei, niiin....
+                    {
+                        if (input == "X" || input == "exit")        //poistuu
+                        {
+                            return;
+                        }
+                        else  // virheilmoitus
+                        {
+                            
+                            Console.WriteLine("\nIncorrect input");
+                            Console.WriteLine("Give integer between 1-21 or X or exit to exit");
+                        }
+
+                    } else
+                    { 
+                        if (theirNumber < 1 || theirNumber > 21)   // Luku ei välillä 1-21:
+                        {
+
+                            Console.WriteLine("\nThe given number is out of limits, try again.");
+                        }
+                        else
+                        {
+                            break;      // luku on välillä 1 - 21, poistutaan silmukasta.
+                        }
+                    }
+
+                }
+                
+                //theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
+                //comparing that given umber is valid
+
+                //comparing
+                if (theirNumber >= myNumber && theirNumber <= 21)
+                {
+                    System.Console.WriteLine("\nYou win.");
+                }
+                else {
+                    System.Console.WriteLine("\nYou lose.");
+                }
         }
-        else {
-          System.Console.WriteLine("You lose.");
-        }
-      }
-    }
+   }
   }
 }
